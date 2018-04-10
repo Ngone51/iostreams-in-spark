56d9ccb6b7e1d90cf4c29e7a095a2e11101ed57e
wuyi <ngone_5451@163.com>
2018-04-10 21:28:23 +0800

    init project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "iostrems-in-spark"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
+        
---
+++ src/main/java/LimitedInputStream.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author wuyi
+ * @date 2018/4/10
+ */
+public class LimitedInputStream {
+}
